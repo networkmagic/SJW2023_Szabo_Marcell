5ab19fb55dfa0c5534e6b1603a5b86fba0502205
Marcell Szabó <marcellszabo912@outlook.com>
2023-03-08 16:22:37 +0100

    Update 1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("elkuld").addEventListener("click", () => {
+    const tipus = document.getElementById("valasz").value;    
+    const darab = document.getElementById("darab").value;
+    if (tipus === '' || darab === '0'){
+        alert("Válasszon ki egy pizzát a listából és írja be a darabszámot!");
+    };
+
+    const valasztott = document.querySelectorAll(":checked");
+    let ar = 0;
+    valasztott.forEach((valaszt) => {
+        ar += valaszt.value;
+    });
+
+    const szam = Math.floor(Math.random() * 100)  + 1;
+    document.getElementById("order").innerText = szam;
+    document.getElementById("total").innerText = tipus * darab + ar;
+    document.getElementById("alert").style.display = "block";
+})
